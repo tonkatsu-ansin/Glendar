33737834f502bdabc530d910b4b8880b543654d4
himanoa <matsunoappy@gmail.com>
2017-01-11 17:19:31 +0900

    WIP component

--- src/components/Interface/ComponentAdd.vue
+++ src/components/Interface/ComponentAdd.vue
@@ -1,29 +1,40 @@
 <template>
-  <form>
-    <input name="id" type="hidden"></input>
-    <input name="image"></input>
-    <input name="memo"></input>
-    <div class="status">
-      <label>hp</label>
-      <input name="hp"></input>
-      <label>maxhp</label>
-      <input name="max_hp"></input>
-      <label>mp</label>
-      <input name="mp"></input>
-      <label>maxmp</label>
-      <input name="max_mp"></input>
-      <label>move</label>
-      <input name="move"></input>
-      <label>protection</label>
-      <input name="protection"></input>
-    </div>
-    <div class="position">
-      <label>x</label>
-      <input name="x"></input>
-      <label>y</label>
-      <input name="y"></input>
-    </div>
-    <input type="submit">OK</input>
-    <input type="submit">Cancel</input>
-  </form>
+  <div class="component-add">
+    <base-dialog>
+      <form>
+        <input name="id" type="hidden"></input>
+        <input name="image"></input>
+        <input name="memo"></input>
+        <div class="status">
+          <label>hp</label>
+          <input name="hp"></input>
+          <label>maxhp</label>
+          <input name="max_hp"></input>
+          <label>mp</label>
+          <input name="mp"></input>
+          <label>maxmp</label>
+          <input name="max_mp"></input>
+          <label>move</label>
+          <input name="move"></input>
+          <label>protection</label>
+          <input name="protection"></input>
+        </div>
+        <div class="position">
+          <label>x</label>
+          <input name="x"></input>
+          <label>y</label>
+          <input name="y"></input>
+        </div>
+        <input type="submit">OK</input>
+        <input type="submit">Cancel</input>
+      </form>
+    </base-dialog>
+  </div>
 </template>
+
+<script>
+module.exports = {
+  components: {
+    "base-dialog": require("./BaseDialog.vue")
+  }
+};
