b4f4919dbf36a248d7fb7cf2d85da95e2e8c6dee
Takuma Hanatani <mail@potato4d.me>
2017-01-11 22:50:02 +0900

    コンポーネントの追加ダイアログを取り敢えず表示されるように

---
+++ src/components/Interface/ComponentaddDialog.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div class="">
+    <base-dialog>
+      
+    </base-dialog>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  components: {
+    "base-dialog": require("./BaseDialog.vue")
+  }
+}
+</script>
